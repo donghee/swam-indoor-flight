9a72268ec820cc64bdc4a0a6632c337d5dccbf0b
donghee <dongheepark@gmail.com>
2019-03-18 11:30:57 +0000

    Delete onboard-odroid-xu4

--- onboard-odroid-xu4/scripts/ssh-key-copy.sh
+++
@@ -1,4 +0,0 @@
-ssh-keygen 
-ssh-copy-id -i ~/.ssh/id_rsa.pub odroid@192.168.88.241
-ssh-copy-id -i ~/.ssh/id_rsa.pub odroid@192.168.88.242
-ssh-copy-id -i ~/.ssh/id_rsa.pub odroid@192.168.88.243
--- onboard-odroid-xu4/setup-network.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# make static ip
-echo "nameserver 8.8.8.8" | tee -a /etc/resolvconf/resolv.conf.d/base
-
-# TODO Change static ip which you want!
-
-cp interfaces /etc/network/
